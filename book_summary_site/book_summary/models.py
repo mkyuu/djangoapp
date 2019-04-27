4dca07f9b24e0eb4c96c1507a5f677f0a5e3bbfe
yuu05 <sintougou@gmail.com>
2019-04-27 16:59:10 +0900

    Revert "テンプレート作成"
    This reverts commit 79ca74d346b3bfb357a66ecd68efffaa131eab74.

--- book_summary_site/book_summary/models.py
+++ book_summary_site/book_summary/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField(blank=True,null=True)
+    text = models.TextField()
     published_date = models.DateTimeField(blank=True,null=True)
 
     def publish(self):
@@ -17,8 +17,6 @@ class Post(models.Model):
 
 class Books(models.Model):
     title = models.CharField(max_length=100)
-    summary = models.CharField(max_length=800)
-    published_date = models.DateTimeField(blank=True,null=True)
 
     def __str__(self):
         return self.title
--- book_summary_site/book_summary/urls.py
+++ book_summary_site/book_summary/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.post_list,name='post_list'),
-    path('books/',views.book_list,name='book_list'),
-    path('books/<int:pk>/',views.book_detail,name='book_detail')
+    path('',views.post_list,name='post_list')
+    path('books',views.book_list,name='book_list')
+    path('books/<int:pk>',views.book_detail,name='book_detail')
 ]
