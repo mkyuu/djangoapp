ca466bd2b7a6a79e193b1a91ca910cca59241bf5
yuu05 <sintougou@gmail.com>
2019-05-07 15:21:58 +0900

    login logout　設定

--- book_summary_site/book_summary_site/settings.py
+++ book_summary_site/book_summary_site/settings.py
@@ -122,4 +122,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = 'accounts:login'
+LOGIN_REDIRECT_URL = 'book_summary:index'
